b745d25da2c02ebfba36102ab293753d158602c7
Max Siew <siew24.dev@gmail.com>
2022-01-24 23:07:20 +0800

    Implemented course enroll and disenroll

--- src/commands/course/enroll.ts
+++ src/commands/course/enroll.ts
@@ -1,5 +1,50 @@
-import { createSubCommand, unimplementedCommandCallback } from '../../utilities.js';
+import { ApplicationCommandOptionChoice, AutocompleteInteraction, CacheType } from 'discord.js';
+import { Course } from '../../models/course.js';
 
-const command = createSubCommand('enroll', 'Enroll into a course', (_) => _, unimplementedCommandCallback());
+import { Guild } from '../../models/guild.js';
+import { createSubCommand } from '../../utilities.js';
+
+const guild = await Guild.get(process.env.GUILD_ID as string);
+
+let courses: Course[] = [];
+
+const command = createSubCommand('enroll', 'Enroll into a course',
+    builder => builder.addStringOption(option => option.setName('course')
+        .setDescription('The course to enroll into')
+        .setRequired(true)
+        .setAutocomplete(true)),
+    async interaction => {
+
+        if (interaction.isAutocomplete()) {
+            courses = await guild.getAllCourses();
+            await (interaction as AutocompleteInteraction).respond((_ => {
+                let result = [];
+                for (let course of courses) {
+                    if (course._students.includes((interaction as AutocompleteInteraction).user.id))
+                        continue;
+
+                    result.push({ name: course._name, value: course._name })
+                }
+
+                return result;
+            })());
+            return;
+        }
+
+        const courseName = interaction.options.getString('course', true);
+
+        const course = courses.find(course => course._name === courseName);
+
+        if (course === undefined) {
+            await interaction.reply({ content: `Sorry, there was a problem finding the given course. It probably didn't exist in the database.\nPlease inform the developers about this problem.`, ephemeral: true });
+            return;
+        }
+
+        course._students.push(interaction.user.id);
+        guild.updateCourse(course);
+        await guild.save();
+
+        await interaction.reply({ content: `You have been enrolled into '${course._name}'!`, ephemeral: true });
+    })
 
 export default command;
--- src/commands/course/leave.ts
+++ src/commands/course/leave.ts
@@ -1,5 +1,49 @@
-import { createSubCommand, unimplementedCommandCallback } from '../../utilities.js';
+import { AutocompleteInteraction, CacheType } from 'discord.js';
+import { Course } from '../../models/course.js';
 
-const command = createSubCommand('leave', 'Leave a course', (_) => _, unimplementedCommandCallback());
+import { Guild } from '../../models/guild.js';
+import { createSubCommand } from '../../utilities.js';
+
+const guild = await Guild.get(process.env.GUILD_ID as string);
+
+let courses: Course[] = [];
+
+const command = createSubCommand('leave', 'Leave a course',
+    builder => builder.addStringOption(option => option.setName('course')
+        .setDescription('The course to leave from')
+        .setRequired(true)
+        .setAutocomplete(true)),
+    async interaction => {
+        const courseName = interaction.options.getString('course', true);
+
+        if (interaction.isAutocomplete()) {
+            courses = await guild.getAllCourses();
+            await (interaction as AutocompleteInteraction).respond((_ => {
+                let result = [];
+                for (let course of courses) {
+                    if (!course._students.includes((interaction as AutocompleteInteraction).user.id))
+                        continue;
+
+                    result.push({ name: course._name, value: course._name })
+                }
+
+                return result;
+            })());
+            return;
+        }
+
+        const course = courses.find(value => value._name === courseName);
+
+        if (course === undefined) {
+            await interaction.reply({ content: `Sorry, there was a problem finding the given course. It probably didn't exist in the database.\nPlease inform the developers about this problem.`, ephemeral: true });
+            return;
+        }
+
+        course._students.filter(student => student !== interaction.user.id);
+        guild.updateCourse(course);
+        await guild.save();
+
+        await interaction.reply({ content: `You have left '${course._name}'!`, ephemeral: true });
+    })
 
 export default command;
